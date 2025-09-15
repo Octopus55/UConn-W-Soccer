8990606265faa4917a3dd3937bbfffa2c6c02dea
Nicholas Pfeifer <nickpfeifer5518@gmail.com>
2025-09-15 14:20:40 -0400

    Added initial files

---
+++ season_comp.py
@@ -0,0 +1,169 @@
+import dash
+from dash import dcc
+from dash import html
+from dash.dependencies import Input, Output
+import plotly.express as px
+import pandas as pd
+import numpy as np
+import warnings
+from soccer_functions import (clean_col_names, filter_team, numeric_cols,
+                              better_add_opp, snake_columns)
+
+warnings.filterwarnings('ignore')
+
+app = dash.Dash(__name__)
+server = app.server
+
+# read in data and clean column names
+fall_2024_stats = pd.read_csv('data/team_stats_uconn_huskies_fall_2024.csv')
+fall_2024_stats = clean_col_names(fall_2024_stats)
+
+fall_2025_stats = pd.read_csv('data/team_stats_uconn_huskies_fall_2025.csv')
+fall_2025_stats = clean_col_names(fall_2025_stats)
+
+# Concatanate 2024 and 2025 together
+season_comp = pd.concat([fall_2025_stats, fall_2024_stats]).reset_index(drop=True)
+
+# Filter to only UConn rows
+season_comp = filter_team(season_comp)
+
+# columns to snake case
+snake_columns(season_comp)
+
+season_comp = season_comp.reset_index(drop=True)
+
+# Engineer year variable
+season_comp['date'] = pd.to_datetime(season_comp['date'])
+season_comp.insert(4, 'year', season_comp['date'].dt.year)
+
+
+# derive result if game is in 2025, else evualuate to '2024'
+season_comp['result'] = np.where(
+    season_comp['goals'] > season_comp['conceded_goals'], 'win',
+    np.where(season_comp['goals'] == season_comp['conceded_goals'], 'draw', 'loss')
+)
+season_comp['result_2025'] = np.where(season_comp['year'] == 2025, season_comp['result'], '2024')
+
+# derive opponent from match column
+my_team = "UCONN Huskies"
+
+# Split match into components
+season_comp[['home_team', 'rest']] = season_comp['match'].str.split(' - ', expand=True)
+season_comp[['away_team', 'score']] = season_comp['rest'].str.extract(r'(.+?) (\d+:\d+)$')
+season_comp.drop(columns='rest', inplace=True)
+
+# Determine opponent
+season_comp['opponent'] = season_comp.apply(
+    lambda row: row['away_team'] if row['home_team'] == my_team else (
+                row['home_team'] if row['away_team'] == my_team else None),
+    axis=1
+)
+
+# Back to title case and rename specific columns
+season_comp.columns = season_comp.columns.str.replace('_', ' ').str.title()
+mapper = {'Passes To Final Third Accurate': 'Final Third Entries',
+          'Ppda': 'PPDA', 'Xg': 'xG'}
+season_comp = season_comp.rename(columns=mapper, inplace=False)
+
+
+app.layout = html.Div(className = 'wrapper', children = [
+    html.H1('Game Comparison Tool', className = 'main-title'),
+    html.H3('How to use:'),
+    html.Ul(children=[html.Li('Select variables for the x and y axis using the dropdowns'),
+                      html.Li('Hover over the individual points for more detailed information'),
+                      html.Li('Games in light gray are from the 2024 season'),
+                      html.Li('Click on criterias in the legend to filter out such points')]),
+    html.Label('Select X Variable', className = 'label'),
+    dcc.Dropdown(season_comp.columns[7:110], id='x-axis', value='Final Third Entries',
+                 style={'width': '50%'}),
+    html.Br(),
+    html.Label('Select Y Variable', className = 'label'),
+    dcc.Dropdown(season_comp.columns[7:110], id='y-axis', value='PPDA',
+                 style={'width': '50%'}),
+    dcc.Graph(id='scatter-plot')
+])
+
+@app.callback(
+    Output('scatter-plot', 'figure'),
+    Input('x-axis', 'value'),
+    Input('y-axis', 'value')
+)
+def update_plot(x_var, y_var):
+    # Define color mapping
+    color_map = {
+        '2024': '#DDDDDD',
+        'win': 'green',
+        'draw': 'gray',
+        'loss': 'red'
+    }
+
+    # Apply color mapping
+    season_comp['color'] = season_comp['Result 2025'].map(color_map)
+
+    # Create label column only for year == 2025
+    season_comp['label'] = season_comp.apply(
+        lambda row: row['Opponent'][:3] if row['Year'] == 2025 and pd.notnull(row['Opponent']) else '',
+        axis=1
+    )
+
+    # Create tooltip text
+    season_comp['tooltip_text'] = season_comp.apply(
+        lambda row: f"{row['Home Team']} {str(row['Score'])[0]} - {row['Away Team']} {str(row['Score'])[2]}",
+        axis=1
+    )
+
+
+    # Create scatter plot
+    fig = px.scatter(
+        season_comp,
+        x=x_var,
+        y=y_var,
+        color=season_comp['Result 2025'],  # for legend grouping
+        color_discrete_map=color_map,
+        text='label',  # this will show labels on hover and optionally on plot
+        hover_name='tooltip_text',
+        hover_data={
+            x_var:True,
+            y_var:True,
+            'label':False,
+            'Result 2025':False
+        },
+        category_orders={'Result 2025': ['win', 'draw', 'loss', '2024']}
+    )
+
+    # Customize label display
+    fig.update_traces(
+        textposition='top center',
+        textfont=dict(size=12),
+        marker=dict(size=12, line=dict(width=0.5, color='black'))
+    )
+
+    # Add axis labels
+    fig.update_layout(
+        xaxis_title=x_var,
+        yaxis_title=y_var,
+        xaxis=dict(
+            showgrid=False,
+            gridcolor='black',
+            ticks='outside',
+            showline=True,
+            linecolor='black'
+        ),
+        yaxis=dict(
+            showgrid=False,
+            gridcolor='black',
+            ticks='outside',
+            showline=True,
+            linecolor='black'
+        ),
+        showlegend=True,
+        legend_title_text='Match Result',
+        plot_bgcolor='#EFEFEF',
+        paper_bgcolor='#FFFFFF',
+        height=500,
+        width=1200
+    )
+    return fig
+
+if __name__ == '__main__':
+    app.run(debug = False)
---
+++ soccer_functions.py
@@ -0,0 +1,118 @@
+import pandas as pd
+import numpy as np
+import warnings
+
+warnings.filterwarnings('ignore')
+
+def clean_col_names(data):
+    clean_cols = data.rename(columns={'Shots / on target': 'Shots',
+                                    'Unnamed: 9': 'Shots On Target',
+                                    'Unnamed: 10': 'Shots On Target Percentage',
+                                    'Passes / accurate': 'Passes Attempted',
+                                    'Unnamed: 12': 'Accurate Passes',
+                                    'Unnamed: 13': 'Pass Accuracy Percentage',
+                                    'Possession, %': 'Possession Percentage',
+                                    'Losses / Low / Medium / High': 'Total Losses',
+                                    'Unnamed: 16': 'Low Losses',
+                                    'Unnamed: 17': 'Medium Losses',
+                                    'Unnamed: 18': 'High Losses',
+                                    'Recoveries / Low / Medium / High': 'Total Recoveries',
+                                    'Unnamed: 20': 'Low Recoveries',
+                                    'Unnamed: 21': 'Medium Recoveries',
+                                    'Unnamed: 22': 'High Recoveries',
+                                    'Duels / won': 'Total Duels',
+                                    'Unnamed: 24': 'Duels Won',
+                                    'Unnamed: 25': 'Duels Win Percentage',
+                                    'Shots from outside penalty area / on target': 'Shots from outside penalty area',
+                                    'Unnamed: 27': 'Shots from outside penalty area on target',
+                                    'Unnamed: 28': 'Shots from outside penalty area on target Percentage',
+                                    'Positional attacks / with shots': 'Total Postional attacks',
+                                    'Unnamed: 30': 'Positional attacks with shots',
+                                    'Unnamed: 31': 'Positional attacks with shots Percentage',
+                                    'Counterattacks / with shots': 'Total Counterattacks',
+                                    'Unnamed: 33': 'Counterattacks with shots',
+                                    'Unnamed: 34': 'Counterattacks with shots Percentage',
+                                    'Set pieces / with shots': 'Total Set pieces',
+                                    'Unnamed: 36': 'Set pieces with shots',
+                                    'Unnamed: 37': 'Set pieces with shots Percentage',
+                                    'Corners / with shots': 'Total Corners',
+                                    'Unnamed: 39': 'Corners with shots',
+                                    'Unnamed: 40': 'Coners with shots Percentage',
+                                    'Free kicks / with shots': 'Total Free kicks',
+                                    'Unnamed: 42': 'Free kicks with shots',
+                                    'Unnamed: 43': 'Free kicks with shots Percentage',
+                                    'Penalties / converted': 'Total Penalties',
+                                    'Unnamed: 45': 'Penalties converted',
+                                    'Unnamed: 46': 'Penalties converted Percentage',
+                                    'Crosses / accurate': 'Total Crosses',
+                                    'Unnamed: 48': 'Crosses accurate',
+                                    'Unnamed: 49': 'Crosses accurate Percentage',
+                                    'Penalty area entries (runs / crosses)': 'Total Penalty area entries',
+                                    'Unnamed: 53': 'Penalty area entries (runs)',
+                                    'Unnamed: 54': 'Penalty area entries (crosses)',
+                                    'Offensive duels / won': 'Total Offensive duels',
+                                    'Unnamed: 57': 'Offensive duels won',
+                                    'Unnamed: 58': 'Offensive duels win Percentage',
+                                    'Shots against / on target': 'Total Shots against',
+                                    'Unnamed: 62': 'Shots against on target',
+                                    'Unnamed: 63': 'Shots against on target Percentage',
+                                    'Defensive duels / won': 'Total Defensive duels',
+                                    'Unnamed: 65': 'Defensive duels won',
+                                    'Unnamed: 66': 'Defensive duels win Percentage',
+                                    'Aerial duels / won': 'Total Aerial duels',
+                                    'Unnamed: 68': 'Aerial duels won',
+                                    'Unnamed: 69': 'Aerial duels win Percentage',
+                                    'Sliding tackles / successful': 'Total Sliding tackles',
+                                    'Unnamed: 71': 'Sliding tackles successful',
+                                    'Unnamed: 72': 'Sliding tackles successful Percentage',
+                                    'Forward passes / accurate': 'Total Forward passes',
+                                    'Unnamed: 79': 'Forward passes accurate',
+                                    'Unnamed: 80': 'Forward passes accurate Percentage',
+                                    'Back passes / accurate': 'Total Back passes',
+                                    'Unnamed: 82': 'Back passes accurate',
+                                    'Unnamed: 83': 'Back passes accurate Percentage',
+                                    'Lateral passes / accurate': 'Total Lateral passes',
+                                    'Unnamed: 85': 'Lateral passes accurate',
+                                    'Unnamed: 86': 'Lateral passes accurate Percentage',
+                                    'Long passes / accurate': 'Total Long passes',
+                                    'Unnamed: 88': 'Long passes accurate',
+                                    'Unnamed: 89': 'Long passes accurate Percentage',
+                                    'Passes to final third / accurate': 'Total Passes to final third',
+                                    'Unnamed: 91': 'Passes to final third accurate',
+                                    'Unnamed: 92': 'Passes to final third accurate Percentage',
+                                    'Progressive passes / accurate': 'Total Progressive passes',
+                                    'Unnamed: 94': 'Progressive passes accurate',
+                                    'Unnamed: 95': 'Progressive passes accurate Percentage',
+                                    'Smart passes / accurate': 'Total Smart passes',
+                                    'Unnamed: 97': 'Smart passes accurate',
+                                    'Unnamed: 98': 'Smart passes accurate Percentage',
+                                    'Throw ins / accurate': 'Total Throw ins',
+                                    'Unnamed: 100': 'Throw ins accurate',
+                                    'Unnamed: 101': 'Throw ins accurate Percentage'
+    })
+    return clean_cols
+
+def filter_team(data, team_name = 'UCONN Huskies'):
+    return data[data['Team'] == team_name]
+
+def numeric_cols(data):
+    return data.iloc[:, 6:]
+
+def better_add_opp(data):
+    # Define the slice of columns to be mirrored
+    target_cols = data.columns[6:109]
+    
+    # Create a copy of the target slice shifted by one position
+    shifted = data[target_cols].shift(-1)
+    
+    # Alternate the shift for odd-indexed rows (so it's symmetrical)
+    opp_data = shifted.copy()
+    opp_data.iloc[1::2] = data[target_cols].shift(1).iloc[1::2]
+    
+    # Rename columns and assign
+    opp_data.columns = ['opp_' + col for col in opp_data.columns]
+    return pd.concat([data, opp_data], axis=1)
+
+def snake_columns(data):
+    data.columns = data.columns.str.lower().str.replace(' ', '_')
+
